b59fe57f2d6dbe1636929eea0b5654e2410d1eb9
Omololu Emmannuel <emmaomoh@gmail.com>
2020-12-29 05:38:10 +0100

    First Session of TDD just ended - Setup Feature test and Highligt what the test entails

---
+++ tests/Feature/ViewAnotherUsersTweets.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ViewAnotherUsersTweetsTest extends TestCase
+{
+
+    public function test_can_view_another_users_tweets()
+    {
+        //A User must exist
+        //User has Tweets
+        //Go to User's Profile
+        //View User's Tweet
+
+    }
+}
