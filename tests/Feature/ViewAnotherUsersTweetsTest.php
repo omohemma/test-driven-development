9739c2a7fb2e6a732a82a59c8e0b13f5c181e76f
Omololu Emmannuel <emmaomoh@gmail.com>
2020-12-31 08:37:31 +0100

    My first test

---
+++ tests/Feature/ViewAnotherUsersTweetsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ViewAnotherUsersTweetsTest extends TestCase
+{
+
+    // Setup database migration
+    use DatabaseMigrations; 
+    
+    public function test_can_view_another_users_tweets()
+    {
+        //A User must exist
+        $user = factory(User::class)->create([
+            'username' => 'johndoe'
+        ]);
+
+        //User has Tweets
+        $tweets = factory(Tweet::class)->create([
+            'body' => 'This is my first tweet',
+        ]);
+        $user->tweets()->save($tweets); // attach tweets to user
+
+        //Go to User's Profile
+            $this->visit('/johndoe')
+            ->see('This is my first tweet');
+        
+        //View User's Tweet
+
+    }
+}
